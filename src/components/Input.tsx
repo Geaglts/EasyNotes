403b41c1db681f3a530c5e2fc6b8c77ee218b50a
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-09-04 15:07:23 -0500

    Componente input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,5 +1,20 @@
-function Input() {
-  return <div></div>;
+import { InputHTMLAttributes } from 'react';
+import { MdSentimentVeryDissatisfied } from 'react-icons/md';
+
+import '../styles/Components/Input.scss';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  Icon?: JSX.Element;
+}
+
+function Input(props: InputProps) {
+  const { Icon, ...rest } = props;
+  return (
+    <div className="Input">
+      <div className="Input__Icon">{Icon || <MdSentimentVeryDissatisfied />}</div>
+      <input className="Input__Input" {...rest}></input>
+    </div>
+  );
 }
 
 export default Input;
--- src/containers/Form.tsx
+++ src/containers/Form.tsx
@@ -1,9 +1,13 @@
+import { MdSpa } from 'react-icons/md';
+
 import Button from '../components/Button';
+import Input from '../components/Input';
 
 function Form() {
   return (
     <div>
       <Button label="Add Resume" />
+      <Input Icon={<MdSpa />} placeholder="Note Title" />
     </div>
   );
 }
