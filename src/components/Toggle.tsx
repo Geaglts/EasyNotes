ad83aef6b07c65d0044f96c198a193baee07c0b8
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-09-10 16:29:51 -0500

    Menu para cambiar el tema

---
+++ src/components/Toggle.tsx
@@ -0,0 +1,11 @@
+import '../styles/Components/Toggle.scss';
+
+function Toggle() {
+  return (
+    <div className="Toggle">
+      <div className="Toggle__Circle"></div>
+    </div>
+  );
+}
+
+export { Toggle };
