aafd81721f4384ba37e349113d21ddfd95516aff
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-10-01 23:52:51 -0500

    Estructura de carpetas para la pagina de free notes

---
+++ src/pages/FreeNotes/containers/MainForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainForm() {
+  return (
+    <main>
+      <p>main form</p>
+    </main>
+  );
+}
+
+export default MainForm;
---
+++ src/pages/FreeNotes/containers/MainList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MainList() {
+  return (
+    <main>
+      <p>main list</p>
+    </main>
+  );
+}
+
+export default MainList;
