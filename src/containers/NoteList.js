e027d67e2fdd3c11332c4a120ad55713f84014b4
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-10-09 18:34:55 -0500

    El buscador de notas ya funciona

--- src/containers/NoteList.js
+++ src/containers/NoteList.js
@@ -1,6 +1,5 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { MdSearch } from 'react-icons/md';
 
 import { allNotes } from '../redux/actions/notes.actions';
 import '../styles/Containers/NoteList.scss';
@@ -13,19 +12,42 @@ import { Message } from '../components/Message';
 import { NoteSkeleton } from '../components/skeletons/Note';
 import Error from '../components/Error';
 
+function filterList(searchedValue) {
+  const lowerSearchedValue = searchedValue.toLowerCase().trim();
+  return (note) => {
+    const { title, content } = note;
+    const lowerTitle = title.toLowerCase();
+    const lowerContent = content.toLowerCase();
+
+    return (
+      lowerTitle.includes(lowerSearchedValue) ||
+      lowerContent.includes(lowerSearchedValue)
+    );
+  };
+}
+
 function NoteList(props) {
   const { notes, loading, error, onLoadNotes } = props;
+  const [searchedValue, setSearchedValue] = useState('');
   const { darkTheme } = useContext(Context);
 
   useEffect(() => {
     onLoadNotes();
   }, []);
 
+  const onChangeSearchButton = (event) => {
+    setSearchedValue(event.target.value);
+  };
+
   return (
     <div className={`NoteList ${darkTheme ? 'NoteListDark' : ''}`}>
       <div className="NoteList__header">
         <h1 className="NoteList__Title">NoteList</h1>
-        <SearchButton inputPlaceholder="Como iba la nota?" />
+        <SearchButton
+          inputPlaceholder="Como iba la nota?"
+          onChange={onChangeSearchButton}
+          value={searchedValue}
+        />
       </div>
       <div className="NoteList__List--Container">
         {loading && <NoteSkeleton />}
@@ -36,7 +58,7 @@ function NoteList(props) {
         {!loading &&
           !error &&
           notes.length > 0 &&
-          notes.map((note) => {
+          notes.filter(filterList(searchedValue)).map((note) => {
             return <Note key={note._id} {...note} />;
           })}
       </div>
