8adf5af64b9996c6dbee83caa759d5dfed160796
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-10-09 16:23:05 -0500

    Vista del formulario en fast note

--- src/pages/FreeNotes/containers/Main.js
+++ src/pages/FreeNotes/containers/Main.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../../Context';
+
 import MainForm from './MainForm';
 import MainList from './MainList';
 
+import '../../../styles/pages/FreeNotes/containers/Main.scss';
+
 function Main() {
+  const { darkTheme } = useContext(Context);
+  const themeClass = darkTheme ? 'freeNotes-dark' : 'freeNotes-white';
+
   return (
-    <main>
+    <main className={`freeNotes__container ${themeClass}`}>
       <MainForm />
       <MainList />
     </main>
--- src/pages/FreeNotes/containers/MainForm.js
+++ src/pages/FreeNotes/containers/MainForm.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import AddNoteForm from '../../../containers/AddNoteForm';
+
+import '../../../styles/pages/FreeNotes/containers/MainForm.scss';
 
 function MainForm() {
   return (
-    <main>
-      <p>main form</p>
+    <main className="mainForm__container">
+      <h1>Crea tus notas fácil y rápido.</h1>
+      <p>
+        Escribe lo que gustes, y si cierras el navegador no te preocupes, tus notas
+        estarán en aquí hasta que tú decidas eliminarlas.
+      </p>
+      <AddNoteForm />
     </main>
   );
 }
