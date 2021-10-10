184d74b0104e6882799e4295d14680a9d2864781
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-10-09 20:40:37 -0500

    Estilos para la version movil

--- src/pages/FreeNotes/containers/MainForm.js
+++ src/pages/FreeNotes/containers/MainForm.js
@@ -5,14 +5,14 @@ import '../../../styles/pages/FreeNotes/containers/MainForm.scss';
 
 function MainForm() {
   return (
-    <main className="mainForm__container">
+    <section className="mainForm__container">
       <h1>Crea tus notas fácil y rápido.</h1>
       <p>
         Escribe lo que gustes, y si cierras el navegador no te preocupes, tus notas
         estarán en aquí hasta que tú decidas eliminarlas.
       </p>
       <AddNoteForm />
-    </main>
+    </section>
   );
 }
 
