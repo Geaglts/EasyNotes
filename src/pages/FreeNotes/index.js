e37b41d7aa4065907ab4192e575d3b933f83a853
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-10-15 18:42:08 -0500

    Cambie algunos estilos para el responsive

--- src/pages/FreeNotes/index.js
+++ src/pages/FreeNotes/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import Main from './containers/Main';
-/* import Container from '../../components/Container';
-import AddNoteForm from '../../containers/AddNoteForm';
-import NoteList from '../../containers/NoteList'; */
-
-/**
- * <Container>
-      <AddNoteForm />
-      <NoteList />
-    </Container>
- */
 
 function FreeNotes() {
   return <Main />;
