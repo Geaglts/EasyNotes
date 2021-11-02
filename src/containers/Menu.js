acec1bf25d241d6777364e0abae6a19ed93cee5d
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-11-01 19:16:38 -0600

    new styles for menu

--- src/containers/Menu.js
+++ src/containers/Menu.js
@@ -3,29 +3,32 @@ import { Context } from '../Context';
 import { Link } from 'react-router-dom';
 import '../styles/Containers/Menu.scss';
 import { FaMoon, FaSun } from 'react-icons/fa';
+import { BiMenu } from 'react-icons/bi';
 
 import { Toggle } from '../components/Toggle';
 
 function Menu() {
   const { darkTheme, changeTheme } = useContext(Context);
 
+  const theme = darkTheme ? ' dark' : ' white';
+
   return (
-    <nav
-      className="Menu__Container"
-      style={{ backgroundColor: darkTheme ? '#141414' : 'rgba(71, 93, 237,.8)' }}
-    >
-      <ul className="Menu__Left">
-        <li className="Menu__Item">
-          <Link to="/">EasyNotes</Link>
-        </li>
-      </ul>
-      <ul className="Menu">
-        <li className="Menu__Item">
+    <nav className={'menu__container' + theme}>
+      <Link to="/" className="menu__container--title">
+        EasyNotes
+      </Link>
+      <ul className="menu__list">
+        <li className="menu__list--item">
           <Link to="/fast">Nota rapida</Link>
         </li>
-        <li className="Menu__Item">
+        <li className="menu__list--item">
           {darkTheme ? <FaMoon /> : <FaSun />}
           <Toggle onClick={changeTheme} status={darkTheme} />
+        </li>
+        <li className="menu__list--item menu__list--button">
+          <button className="show_menu">
+            <BiMenu />
+          </button>
         </li>
       </ul>
     </nav>
