e4e99a71cf053596236110d6debf8dbf6b785e21
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2021-12-03 22:10:10 -0600

    [new] login and register schema

---
+++ src/schemas/register.schema.js
@@ -0,0 +1,10 @@
+import * as Yup from 'yup';
+
+export const registerSchema = Yup.object().shape({
+  firstName: Yup.string().required('Su nombre es requerido'),
+  lastName: Yup.string().required('Sus apellidos son requeridos'),
+  email: Yup.string().email('Este no es un correo electrónico válido').required('Su correo es requerido'),
+  alias: Yup.string().min(3, 'El alias debe tener al menos 3 caracteres'),
+  password: Yup.string().min(8, 'Su contraseña debe tener al menos 8 caracteres').required('Su contraseña es requerida'),
+  passwordToCompare: Yup.string().required('Su contraseña es requerida'),
+});
