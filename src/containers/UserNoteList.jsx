5e6ddedbbb6efbc35a6a47a86504e4af470ba685
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-01-10 13:58:35 -0600

    Add Search bar in user note list

--- src/containers/UserNoteList.jsx
+++ src/containers/UserNoteList.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { UserNote } from 'components/Note';
 
 import 'styles/Containers/UserNoteList.scss';
+import FormControl from 'utils/classes/FormControl';
+
+const filterNotes =
+  (noteSearched = '') =>
+  (note) => {
+    const { title } = FormControl.decryptData({ title: note.title });
+    const parsedTitle = title.trim().toLowerCase();
+    if (parsedTitle.includes(noteSearched)) {
+      return true;
+    }
+    return false;
+  };
 
 export const UserNoteList = ({ notes = [] }) => {
+  const [noteSearched, setNoteSearched] = useState('');
+
+  const noteListMemo = useMemo(() => notes.filter(filterNotes(noteSearched)), [noteSearched]);
+
+  const onChangeNoteSearched = (e) => {
+    setNoteSearched(e.target.value);
+  };
+
   return (
-    <div className="UserNoteList">
-      {notes.map((note) => (
-        <UserNote {...note} key={note.id} />
-      ))}
-    </div>
+    <>
+      <div className="UserNoteList">
+        <div className="UserNoteList_SearchBar">
+          <input type="text" placeholder="nombre de la nota..." onChange={onChangeNoteSearched} />
+        </div>
+        <div className="UserNoteList_NoteList">
+          {noteListMemo.map((note) => (
+            <UserNote {...note} key={note.id} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
