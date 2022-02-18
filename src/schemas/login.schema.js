fe98dfc5513f0c6b57f089d317b09d247a5d95e8
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-02-18 02:30:06 -0600

    Update login validation schema

--- src/schemas/login.schema.js
+++ src/schemas/login.schema.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
 export const loginSchema = Yup.object().shape({
-  email: Yup.string().email('Este correo no es valido').required('El correo es requerido'),
+  email: Yup.string().required('El correo es requerido'),
   password: Yup.string().required('La contraseña es requerida'),
 });
