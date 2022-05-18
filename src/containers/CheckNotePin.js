a50ab27eb06fa4cb5950188934dc13d9550f0639
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-05-18 15:00:19 -0500

    [new]: use password to unlock note if user forgot her pin

--- src/containers/CheckNotePin.js
+++ src/containers/CheckNotePin.js
@@ -1,7 +1,10 @@
 import React, { useRef, useState } from 'react';
+import axios from 'axios';
+
 import Modal from 'components/Modal';
 import { SimpleInput } from 'components/Input';
 import Button from 'components/Button';
+
 import FormControl from 'utils/classes/FormControl';
 
 import styles from 'styles/Containers/CheckNotePin.module.scss';
@@ -9,7 +12,12 @@ import styles from 'styles/Containers/CheckNotePin.module.scss';
 const CheckNotePin = ({ pin, children, visibility, changeVisibility }) => {
   const [callback, setCallback] = useState(null);
   const [showError, setShowError] = useState(false);
+  const [forgotPin, setForgotPin] = useState(false);
   const pinInput = useRef(null);
+
+  const toggleFotgotPin = () => {
+    setForgotPin(!forgotPin);
+  };
 
   const showValidation = (callback) => (evt) => {
     if (!pin) {
@@ -20,11 +28,22 @@ const CheckNotePin = ({ pin, children, visibility, changeVisibility }) => {
     }
   };
 
-  const verifyPin = (event) => {
+  const verifyPin = async (event) => {
     event.preventDefault();
-    const { pin: decrypedPin } = FormControl.decryptData({ pin });
-    if (pinInput.current.value === decrypedPin) {
+    const inputValue = pinInput.current.value;
+    let hasAuthorization = false;
+    if (!forgotPin) {
+      const { pin: decrypedPin } = FormControl.decryptData({ pin });
+      hasAuthorization = inputValue === decrypedPin;
+    } else {
+      try {
+        const { data } = await axios.post('/api/v1/users/password-validation', { password: inputValue });
+        hasAuthorization = data;
+      } catch {}
+    }
+    if (hasAuthorization) {
       changeVisibility();
+      toggleFotgotPin();
       callback.run();
     } else {
       setShowError(true);
@@ -37,10 +56,13 @@ const CheckNotePin = ({ pin, children, visibility, changeVisibility }) => {
   return (
     <>
       {children(showValidation)}
-      <Modal title="Verificación de PIN" active={visibility} changeStatus={changeVisibility}>
+      <Modal title={`Verificación  de ${forgotPin ? 'Contraseña' : 'PIN'}`} active={visibility} changeStatus={changeVisibility}>
         <div className={styles.Container}>
-          <SimpleInput ref={pinInput} placeholder="Ingresa el PIN de tu Nota" />
+          <SimpleInput ref={pinInput} placeholder={`Ingresa ${forgotPin ? 'tu Contraseña' : 'el PIN de tu Nota'}`} />
           <Button label="Abrir" onClick={verifyPin} classNames={[styles.Button]} />
+          <button className={styles.ButtonForgotPin} onClick={toggleFotgotPin}>
+            Desbloquear {forgotPin ? 'con PIN' : 'con Contraseña'}
+          </button>
           {showError && <p className={styles.Error}>Este pin es incorrecto</p>}
         </div>
       </Modal>
