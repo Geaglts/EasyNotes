1cf0e3d134f90cadc76a45ee5b026c8de68caad2
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2022-11-20 22:00:43 -0600

    fix syle in loginwith component

--- src/components/LoginWith.js
+++ src/components/LoginWith.js
@@ -2,12 +2,10 @@ import styles from '@styles/Components/LoginWith.module.scss';
 
 const LoginWith = ({ to, provider, providerLogo }) => {
   return (
-    <div className={styles.container}>
+    <a className={styles.container} href={to}>
       <img className={styles.providerLogo} src={providerLogo} />
-      <a className={styles.provider} href={to}>
-        Ingresar con {provider}
-      </a>
-    </div>
+      <span className={styles.provider}>Ingresar con {provider}</span>
+    </a>
   );
 };
 
